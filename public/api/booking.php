643cd0fe5482522a119e1cc58feffe08cb4ab515
youh4ck3dme <h4ck3d@h4ck3d.me>
2025-12-23 21:14:29 +0100

    feat: Add POST and OPTIONS methods to booking API CORS headers and introduce a new HTML page for CORS testing.

--- public/api/booking.php
+++ public/api/booking.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=utf-8");
 
